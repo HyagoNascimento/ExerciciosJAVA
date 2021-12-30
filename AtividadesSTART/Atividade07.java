a6f89608d69c734cf2464e0332b0abf55ac9d1d3
Hyago Nascimento <79670510+HyagoNascimento@users.noreply.github.com>
2021-12-30 19:14:13 -0300

    Create Atividade07.java

---
+++ AtividadesSTART/Atividade07.java
@@ -0,0 +1,11 @@
+public class Atividade03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for ( i=1000; i < 2000; i++) {
+			if(i%11==5) {
+				System.out.println(i);
+			}}
+}}
