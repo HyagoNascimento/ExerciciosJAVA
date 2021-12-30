a0b6d4c39229ba275dafd39300c78a3f324f1623
Hyago Nascimento <79670510+HyagoNascimento@users.noreply.github.com>
2021-12-30 19:11:49 -0300

    Create Atividade04.java

---
+++ AtividadesSTART/Atividade04.java
@@ -0,0 +1,23 @@
+public class atividade02_q04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.print("================\n");
+	System.out.print("Mês-Nome do Mês\n");
+	System.out.print("================\n");
+	System.out.print("01 - Janeiro\n");
+	System.out.print("02 - Fevereiro\n");
+	System.out.print("03 - Março\n");
+	System.out.print("04 - Abril\n");
+	System.out.print("05 - Maio\n");
+	System.out.print("06 - Junho\n");
+	System.out.print("07 - Julho\n");
+	System.out.print("08 - Agosto\n");
+	System.out.print("09 - Setembro\n");
+	System.out.print("10 - Outubro\n");
+	System.out.print("11 - Novembro\n");
+	System.out.print("12 - Dezembro\n");
+	System.out.print("================\n");
+	}
+}
