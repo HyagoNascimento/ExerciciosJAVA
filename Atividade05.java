eeea91d177f9bd030ba6edbe8a6179fc7a81eb97
Hyago Nascimento <79670510+HyagoNascimento@users.noreply.github.com>
2021-12-30 18:17:06 -0300

    Create Atividade05.java

---
+++ Atividade05.java
@@ -0,0 +1,26 @@
+//5 – Pedi o a informação do salário do cliente e fiz um cálculo simples, dividindo o salário pelo salário mínimo.
+
+public class Atividade05 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		float salario,salarioTotal;
+		System.out.println("Informe o seu salário: ");
+		salario=  ler.nextFloat();
+		salarioTotal=salario/1100;
+		System.out.printf("%.2f",salarioTotal);System.out.println(	"SM =R$"+salario);
+	}}
+6 – Pedi o número, e subtrai 1 para o antecessor e somei 1 para o sucessor.
+public class Atividade06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		int n;
+		
+		System.out.println("Informe um número: ");
+		n =  ler.nextInt();
+		
+		System.out.println(	"Antecessor de "+n +"=" + (n-1));
+		System.out.println(	"Sucessor de "+n +"=" + (n+1));
+	}}
